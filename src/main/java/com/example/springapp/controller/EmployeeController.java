9917c25d5706b0e09b43058506d146cdb867e5e8
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-10-27 23:32:24 +0530

    fix url origin error

--- src/main/java/com/example/springapp/controller/EmployeeController.java
+++ src/main/java/com/example/springapp/controller/EmployeeController.java
@@ -18,7 +18,7 @@ public class EmployeeController {
    private EmployeeRepository employeeRepository;
 
     //get employees
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
        return employeeRepository.findAll();
